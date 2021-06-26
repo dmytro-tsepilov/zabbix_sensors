0c538332626d16463752c2f1635ea02b2470df91
Dmytro Tsepilov <dmytro.tsepilov@gmail.com>
2021-06-26 20:49:38 +0300

    - Fixed hardcode for temp reader

--- temp_read.sh
+++ temp_read.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+SCRIPT_PATH=$(dirname "$0")
+
 DEVICE=$1
 PROPERTY=$2
 
-cat /home/dimon/zabbix/temperature.txt | grep "/dev/$DEVICE/$PROPERTY" | awk 'BEGIN { FS = ":" }; {printf $2}'
+cat $SCRIPT_PATH/temperature.txt | grep "/dev/$DEVICE/$PROPERTY" | awk 'BEGIN { FS = ":" }; {printf $2}'
